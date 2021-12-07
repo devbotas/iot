a61fc2c7ba6f1646a5bf478f1b4a166cf49566b0
devbotas <simon@devbot.lt>
2021-12-07 10:13:09 +0200

    Initial support for ST7735.

---
+++ src/devices/St7735/Orientation.cs
@@ -0,0 +1,31 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Iot.Device.St7735
+{
+    /// <summary>
+    /// Orientation of the display.
+    /// </summary>
+    public enum Orientation
+    {
+        /// <summary>
+        /// Native orientation, as designed by the manufacturer.
+        /// </summary>
+        Normal,
+
+        /// <summary>
+        /// Rotated CW by 90 degrees.
+        /// </summary>
+        Rotated90,
+
+        /// <summary>
+        /// Rotated CW by 180 degrees.
+        /// </summary>
+        Rotated180,
+
+        /// <summary>
+        /// Rotated CW by 270 degrees.
+        /// </summary>
+        Rotated270
+    }
+}
---
+++ src/devices/St7735/samples/St7735.Sample.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Device.Spi;
+using System.Threading;
+using Iot.Device.St7735;
+
+Console.WriteLine("Hello, World!");
+
+var spiSettings = new SpiConnectionSettings(0, 1);
+spiSettings.ClockFrequency = 10_000_000;
+
+var spiBus = SpiDevice.Create(spiSettings); // Ft4222Spi(new SpiConnectionSettings(0, 1) { ClockFrequency = 1_000_000, Mode = SpiMode.Mode0 });
+
+var lcd = new PimoroniOled(spiBus, 9);
+
+var randomNumber = new Random();
+var whiteRectangleBuffer = new byte[10 * 10];
+for (int i = 0; i < 10 * 10; i++)
+{
+    whiteRectangleBuffer[i] = 0xFF;
+}
+
+while (true)
+{
+    Thread.Sleep(10);
+
+    var randomX = randomNumber.Next(lcd.ActualWidth - 10);
+    var randomY = randomNumber.Next(lcd.ActualHeight - 10);
+
+    lcd.SetRegion(randomX, randomY, 10, 10);
+    lcd.SendBitmap(whiteRectangleBuffer);
+}
